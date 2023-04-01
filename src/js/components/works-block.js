82820705087bb9f61616fcca16142d896f6bc983
n1colashka <deckards2505@gmail.com>
2023-04-01 20:28:11 +0600

    fixes

--- src/js/components/works-block.js
+++ src/js/components/works-block.js
@@ -14,8 +14,11 @@ workItems.forEach(el => {
 	const dragItem = interact(el.querySelector('.works__drag'));
 	const dragIcon = el.querySelector('.works__drag-icon');
 	const dragLine = el.querySelector('.works__drag-line');
+	const captions = el.querySelectorAll('.works__captions-item');
 	const captionBefore = el.querySelector('.works__captions-item--before');
 	const captionAfter = el.querySelector('.works__captions-item--after');
+	const minValue = 2.5;
+	const maxValue = 97.5;
 
 	dragItem
   .draggable({ 
@@ -32,31 +35,49 @@ workItems.forEach(el => {
         const value = (event.pageX / sliderWidth);
 				let percent = value * 100;
 
-				if ((value * 100) > 98) {
-					percent = 97.5;
+				if ((value * 100) >= maxValue) {
+					percent = maxValue;
 					return;
-				} else if ((value * 100) <= 2) {
-					percent = 2.5;
+				} else if ((value * 100) <= minValue) {
+					percent = minValue;
 					return;
 				}
-				dragIcon.style.left = percent + '%';
-				dragLine.style.left = percent + '%';
-				image.style.clipPath = `polygon(0 0, ${percent}% 0, ${percent}% 100%, 0% 100%)`;
+			
+				setDragStyles(percent);
 
 				if (percent < 50) {
-					captionBefore.classList.add('is-active');
-					captionBefore.classList.add('is-current');
-					captionAfter.classList.remove('is-active');
-					captionAfter.classList.remove('is-current');
+					setActiveBeforeCaption(percent);
 				} else {
-					captionBefore.classList.remove('is-active');
-					captionBefore.classList.remove('is-current');
-					captionAfter.classList.add('is-active');
-					captionAfter.classList.add('is-current');
+					setActiveAfterCaption(percent);
 				}
       }
     }
   })
+
+	captionBefore.addEventListener('click', () => setActiveBeforeCaption(minValue));
+	captionAfter.addEventListener('click', () => setActiveAfterCaption(maxValue));
+
+	function setDragStyles(percent) {
+		dragIcon.style.left = percent + '%';
+		dragLine.style.left = percent + '%';
+		image.style.clipPath = `polygon(0 0, ${percent}% 0, ${percent}% 100%, 0% 100%)`;
+	}
+
+	function setActiveBeforeCaption(percent) {
+		captionBefore.classList.add('is-active');
+		captionBefore.classList.add('is-current');
+		captionAfter.classList.remove('is-active');
+		captionAfter.classList.remove('is-current');
+		setDragStyles(percent);
+	}
+	function setActiveAfterCaption(percent) {
+		captionBefore.classList.remove('is-active');
+		captionBefore.classList.remove('is-current');
+		captionAfter.classList.add('is-active');
+		captionAfter.classList.add('is-current');
+		setDragStyles(percent);
+	}
+
 })
 
 
