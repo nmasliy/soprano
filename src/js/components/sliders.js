5d6917dcab24c8ead4c075f5fa5fcc3f16f27916
n1colashka <deckards2505@gmail.com>
2023-04-01 08:15:31 +0600

    add certificates animation

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,5 +1,17 @@
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// Swiper.use([Navigation, Pagination]);
-// const swiper = new Swiper(el, {
-//   slidesPerView: 'auto',
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+Swiper.use([Navigation, Pagination, Autoplay]);
+
+// const swiper = new Swiper('.certificates__line', {
+// 	direction: 'vertical',
+// 	spaceBetween: 20,
+//   speed: 5000,
+//   loop: true,
+// 	simulateTouch: false,
+//   //allowTouchMove: false, // можно ещё отключить свайп
+//   autoplay: {
+//     delay: 0,
+//     disableOnInteraction: false // или сделать так, чтобы восстанавливался autoplay после взаимодействия
+//   },
+// 	slidesPerView: 'auto',
+// 	freeMode: true,
 // });
