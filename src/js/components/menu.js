fc01f36b16d6f46bafdf1cc22f12f3a6354a9d63
Nikolay Masliy <deckards2505@gmail.com>
2023-04-07 00:35:51 +0600

    fixes

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -4,3 +4,20 @@ import '../functions/languages-select';
 import { getHeaderHeight } from '../functions/header-height';
 
 getHeaderHeight();
+
+if (window.innerWidth <= 1024) {
+  initScrollToServices();
+}
+
+function initScrollToServices() {
+  const menuItems = document.querySelectorAll('.menu__item--services a');
+
+  menuItems.forEach(el => {
+    el.addEventListener('click', function() {
+      const id = el.getAttribute('href').replace('#', '');
+      const tab = document.querySelector(`.services-tabs__item[id="${id}"]`);
+      
+      tab.click();
+    })
+  })
+}
--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -3,4 +3,11 @@ import SmoothScroll from 'smooth-scroll';
 const scroll = new SmoothScroll('a[href*="#"]', {
   speedAsDuration: true,
   duration: 500,
+  offset: function (anchor, toggle) {
+		if (toggle.closest('.menu__item')) {
+			return 170;
+		} else {
+			return 0;
+		}
+	},
 });
